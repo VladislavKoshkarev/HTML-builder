0a69d785338d4a537695b824ef53198c50dc03a1
Vladislav Koshkarev <dddvvv.dv@gmail.com>
2023-05-06 13:46:47 +0300

    refactor: add missing semicolons

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,11 +2,11 @@ function copyDir() {
   const fsPromises = require('fs/promises');
   const path = require('path');
   (async () => {
-    await fsPromises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true })
+    await fsPromises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
     const originalFiles = await fsPromises.readdir(path.join(__dirname, 'files'), {withFileTypes: true});
     for (const file of originalFiles) {
-      fsPromises.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name))
+      fsPromises.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name));
     }
   })();
 }
-copyDir()
+copyDir();
